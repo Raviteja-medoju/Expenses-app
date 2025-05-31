f7f41cd7eb068ae64954e60b8d4ed527afba5c89
Raviteja Medoju <ravitejamedoj@gmail.com>
2025-05-31 17:49:25 -0400

    First Push

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filterExpenses} />
+        <ExpensesList items={filterExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setShowForm(false);
+  };
+
+  const showFormHandler = () => {
+    setShowForm(true);
+  };
+
+  const stopShowFormHandler = () => {
+    setShowForm(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!showForm && <button onClick={showFormHandler}>Add New Expense</button>}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopShowFormHandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
